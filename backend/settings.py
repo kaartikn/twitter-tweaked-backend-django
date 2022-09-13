0121c696003e9bfbc3199344934d104d6eb70723
Kaartik <kaartik2000@gmail.com>
2022-09-14 00:59:36 +0100

    Adjusted texts to allow for venv to run on Mac

--- backend/settings.py
+++ backend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from django.core.management.commands.runserver import Command as runserver
 import environ
+import pymysql  
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
